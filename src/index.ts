42616afdb3b9bb863ecb696c353a64d7d4cda549
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-20 12:57:43 -0700

    adding the delete method

--- src/index.ts
+++ src/index.ts
@@ -53,9 +53,11 @@ app.get("/api/users", (req: Request, res: Response) => {
 });
 
 // Post request
-app.post("/api/users/:id", (req: Request, res: Response) => {
+app.post("/api/users/", (req: Request, res: Response) => {
   const {body} = req;
   const parseId = parseInt(req.params.id);
+  const newUser = {id: users[users.length - 1].id + 1, ...body};
+  users.push(...users, newUser);
   return res.status(201).send({msg: users});
 });
 
@@ -76,10 +78,34 @@ app.put("/api/users/:id", (req: Request, res: Response) => {
 });
 
 // Patch query
-app.patch("/api/users", (req: Request, res: Response) => {
+app.patch("/api/users/:id", (req: Request, res: Response) => {
   const {body} = req;
+  const parseId = parseInt(req.params.id);
+  if (isNaN(parseId)) return res.status(400).send({msg: "Bad request"});
 
-  return res.status(201).send({msg: users});
+  const findUserIndex = users.findIndex((user) => user.id === parseId);
+  if (findUserIndex === -1) return res.status(404).send({msg: "Not found"});
+
+  users[findUserIndex] = {...users[findUserIndex], ...body};
+
+  return res.sendStatus(201).send({msg: "user updated"});
+});
+
+// Delete request
+app.delete("/api/users/:id", (req: Request, res: Response) => {
+  const {id} = req.params;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) return res.status(400).send({msg: "Bad request"});
+
+  const findUserIndex = users.findIndex((user) => user.id === parseId);
+
+  if (findUserIndex === -1) {
+    return res.sendStatus(404).send({msg: "User not found"});
+  }
+
+  users.splice(findUserIndex, 1);
+
+  return res.sendStatus(200).send({msg: "user removed"});
 });
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
