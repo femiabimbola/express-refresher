339f6552195a5ebfdf9ac1cebc592fa48b728772
Femi Abimbola <femiabimbola6@gmail.com>
2024-04-11 04:19:39 -0700

    starting session

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from "express";
+import session from "express-session";
 import routes from "./routes";
 import cookieParser from "cookie-parser";
 
@@ -6,12 +7,26 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "the supposed secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: any, res: Response) => {
+  console.log(req.session);
+  console.log(req.session.id);
+  req.session.visited = true;
   res.cookie("hello", "world", {maxAge: 6000 * 600});
+
   res.status(200).send({msg: "Express with a TypeScript Server"});
 });
 
