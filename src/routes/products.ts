6dcfa948996ee3016183a9bfebf279a00711031e
Femi Abimbola <femiabimbola6@gmail.com>
2024-04-12 12:09:03 -0700

    adding session

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,9 +3,8 @@ import {Request, Response, NextFunction, Router} from "express";
 const router = Router();
 
 router.get("/api/products", (req: any, res: Response) => {
-  // console.log(req.headers.cookie);
-  // console.log(req.cookies);
-
+  console.log(req.session);
+  console.log(req.session.id);
   if (req.cookies.hello && req.cookies.hello === "world") {
     return res.status(200).send({id: 1, name: "Amala ", price: 300});
   }
