4db4713e06a7a944c64894e9cc41bbb80a279b1f
Femi Abimbola <femiabimbola6@gmail.com>
2024-04-14 15:12:25 -0700

    writing user auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,14 @@
 import {Request, Response, NextFunction, Router} from "express";
+import {users} from "../db/users";
 
 const router = Router();
 
-router.post("/api/auth/", (req: Request, res: Response) => {});
+router.post("/api/auth/", (req: Request, res: Response) => {
+  const {password, display} = req.body;
+  const findUser = users.find((user) => user.display === display);
+  if (!findUser) return res.status(401).send({msg: "Credential not found"});
+  if (findUser.password === password)
+    return res.status(401).send({msg: "Password not correct"});
+});
 
 export default router;
