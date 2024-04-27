a935432936cfcaf6dfbeaac8dd5209b654b7e9d8
Femi Abimbola <femiabimbola6@gmail.com>
2024-04-27 05:49:50 -0700

    session store rounding up

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import mongoose from "mongoose";
 const app = express();
 
 app.use(express.json());
+connectToDB();
 
 app.use(cookieParser());
 app.use(
@@ -20,9 +21,9 @@ app.use(
     cookie: {
       maxAge: 60000 * 60,
     },
-    // store: MongoStore.create({
-    //   client: mongoose.connection.getClient(),
-    // }),
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    }),
   })
 );
 app.use(passport.initialize());
@@ -30,8 +31,6 @@ app.use(passport.session());
 app.use(routes);
 
 const PORT = process.env.PORT || 8000;
-
-connectToDB();
 
 app.get("/", (req: any, res: Response) => {
   req.session.visited = true;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,10 +6,10 @@ import "../strategies/localStrategy2";
 
 const router = Router();
 
-// user signin on localdb
+// user signin on localdb using passport
 router.post(
   "/api/auth/login",
-  passport.authenticate("local"),
+  passport.authenticate("local"), //using localstrategy
   (req: any, res: Response) => {
     res.status(201).send({msg: "successfully log in"});
   }
@@ -18,29 +18,13 @@ router.post(
 // Mongodb signin
 router.post(
   "/api/auth/v1/login",
-  passport.authenticate("local"),
+  passport.authenticate("local"), //using localstrategy2
   (req: any, res: Response) => {
     res.status(201).send({msg: "successfully log in"});
   }
 );
 
-// passport user
-router.get("/api/auth", (req: Request, res: Response) => {
-  // console.log(req.session);
-  if (!req.user)
-    return res.status(200).send({msg: "Sign in or create an account"});
-  return res.status(200).send({msg: req.user});
-});
-
-// Passport logout
-router.get("/api/auth/logout", (req: Request, res: Response) => {
-  if (!req.user) return res.status(401).send({msg: "not signed in"});
-  req.logOut((error) => {
-    if (error) return res.status(401).send({msg: "not signed in"});
-    return res.status(200).send({msg: "logged out"});
-  });
-});
-
+// Localdb signin without passport
 router.post("/api/v1/auth/", (req: any, res: Response) => {
   const {password, display} = req.body;
   const findUser = users.find((user) => user.display === display);
@@ -53,10 +37,29 @@ router.post("/api/v1/auth/", (req: any, res: Response) => {
   return res.status(200).send({msg: "Loggimg in"});
 });
 
+// Get passport user any user
+router.get("/api/auth", (req: Request, res: Response) => {
+  console.log(req.session);
+  if (!req.user)
+    return res.status(200).send({msg: "Sign in or create an account"});
+  return res.status(200).send({msg: req.user});
+});
+
+// get user without passport
 router.get("/api/v1/auth/", (req: any, res: Response) => {
   if (!req.session.user) {
     return res.status(200).send({msg: "Not unauthenticated"});
   }
   return res.status(200).send({msg: req.session.user});
 });
+
+// Passport logout
+router.get("/api/auth/logout", (req: Request, res: Response) => {
+  if (!req.user) return res.status(401).send({msg: "not signed in"});
+  req.logOut((error) => {
+    if (error) return res.status(401).send({msg: "not signed in"});
+    return res.status(200).send({msg: "logged out"});
+  });
+});
+
 export default router;
